66506fcf9bfe9b101b87019cd79571b569fbe58d
Paul Fomitchev <paul.fomitchev@gmail.com>
2026-01-16 02:37:02 -0500

    cleaned main

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,6 @@ from sudoku_solver.solver import solve_latin
 from sudoku_solver.sudoku_validator import is_valid_sudoku
 from sudoku_display.main import solution_to_image
 import cv2
-img1 = cv2.imread('/Users/paulfomitchev/Documents/Coding/cv-sudokusolver/backend/datasetsudoku/test/images/sudoku_3288_jpg.rf.5aff634f24dfc3f4ceed048be7bee9a9.jpg')
 
 def solve_sudoku_image(img, output_path="solved_sudoku.jpg"):
     puzzle_strings = process_sudoku(img)
@@ -19,4 +18,4 @@ def solve_sudoku_image(img, output_path="solved_sudoku.jpg"):
     solution_strings = [''.join(map(str, row)) for row in solution]
     solution_to_image(solution_strings, output_path)
 
-solve_sudoku_image(img1)
+
